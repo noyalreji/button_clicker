a91664fa6ea17bb6f62a357dc456ef2fa3fbd0e7
Noyal Jacob <johndoe@example.comNoyal>
2021-07-01 00:46:20 -0500

    pop up box appears when cliecked on the button

---
+++ button.js
@@ -0,0 +1,12 @@
+function turnOff(element) {
+  element.innerText = "Logout";
+}
+
+function hide(element) {
+  element.remove();
+}
+
+function custom() {
+  // we can include more code here if we want to
+  console.log("Ninja was liked");
+}
